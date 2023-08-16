6febf2b1078566b31fd0041272060a233f5fe513
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-08-16 09:50:05 +0900

    テストコードをリファクタした

--- main_test.go
+++ main_test.go
@@ -3,20 +3,13 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
 )
 
-func countFileLines(t *testing.T, filename string) int {
-	t.Helper() // この関数がヘルパーであることを示す
-
-	file, err := os.Open(filename)
-	if err != nil {
-		t.Fatalf("Failed to open file: %v", err)
-	}
-	defer file.Close()
+func countFileLines(t *testing.T, file *os.File) int {
+	t.Helper()
 
 	scanner := bufio.NewScanner(file)
 	lineCount := 0
@@ -30,199 +23,114 @@ func countFileLines(t *testing.T, filename string) int {
 
 	return lineCount
 }
-
-func TestLineSplit(t *testing.T) {
-	input := "line1\nline2\nline3\n"
-	reader := bytes.NewBufferString(input)
-	mode := Line{UnitLineCount: 2}
-
-	d := t.TempDir()
-
-	// 分割実行
-	if err := mode.Split(reader, d); err != nil {
-		t.Errorf("Failed to split: %v", err)
-	}
-
-	// 生成されたファイル
-	files, err := os.ReadDir(d)
-	if err != nil {
-		t.Errorf("Fail os.ReadDir %+v", err)
-	}
-
-	// ファイルの期待値
-	wantFiles := []struct {
+func TestSplit(t *testing.T) {
+	splitTests := []struct {
 		name      string
-		lineCount int
+		input     string
+		mode      Mode
+		wantFiles []struct {
+			name      string
+			byteCount int64
+			lineCount int
+		}
 	}{
 		{
-			name:      "part-line-1",
-			lineCount: 2,
+			name:  "LineSplit",
+			input: "line1\nline2\nline3\n",
+			mode:  Line{UnitLineCount: 2},
+			wantFiles: []struct {
+				name      string
+				byteCount int64
+				lineCount int
+			}{
+				{name: "part-line-1", byteCount: 12, lineCount: 2},
+				{name: "part-line-2", byteCount: 6, lineCount: 1},
+			},
 		},
 		{
-			name:      "part-line-2",
-			lineCount: 1,
+			name:  "ByteSplit",
+			input: "abcdefghijklmn",
+			mode:  Byte{UnitByteCount: 4},
+			wantFiles: []struct {
+				name      string
+				byteCount int64
+				lineCount int
+			}{
+				{name: "part-byte-1", byteCount: 4, lineCount: 1},
+				{name: "part-byte-2", byteCount: 4, lineCount: 1},
+				{name: "part-byte-3", byteCount: 4, lineCount: 1},
+				{name: "part-byte-4", byteCount: 2, lineCount: 1},
+			},
+		},
+		{
+			name:  "ChunkSplit",
+			input: "abcdefghijklmn",
+			mode:  Chunk{UnitChunkCount: 2},
+			wantFiles: []struct {
+				name      string
+				byteCount int64
+				lineCount int
+			}{
+				{name: "part-num-1", byteCount: 7, lineCount: 1},
+				{name: "part-num-2", byteCount: 7, lineCount: 1},
+			},
 		},
 	}
 
-	// ファイルの件数の確認
+	for _, tt := range splitTests {
+		t.Run(tt.name, func(t *testing.T) {
+			reader := bytes.NewBufferString(tt.input)
+			d := t.TempDir()
+
+			if err := tt.mode.Split(reader, d); err != nil {
+				t.Errorf("Failed to split: %v", err)
+			}
+
+			checkFiles(t, d, tt.wantFiles)
+		})
+	}
+}
+
+// ファイルの内容をチェックする共通関数
+func checkFiles(t *testing.T, dir string, wantFiles []struct {
+	name      string
+	byteCount int64
+	lineCount int
+}) {
+	t.Helper()
+
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		t.Fatalf("Failed to read dir: %v", err)
+	}
+
 	if len(files) != len(wantFiles) {
 		t.Errorf("mismatch the number of files, got=%d, want=%d", len(files), len(wantFiles))
 	}
 
-	// ファイル名確認
 	for i, f := range files {
 		if f.Name() != wantFiles[i].name {
-			t.Errorf("files[%d], %s != %s", i, f.Name(), wantFiles[i].name)
+			t.Errorf("file name mismatch at index %d: got=%s, want=%s", i, f.Name(), wantFiles[i].name)
 		}
-	}
 
-	// ファイルの行数を確認する
-	for i, f := range files {
-		lineCount := countFileLines(t, filepath.Join(d, f.Name()))
+		file, err := os.Open(filepath.Join(dir, f.Name()))
+		if err != nil {
+			t.Fatalf("Failed to open file: %v", err)
+		}
+		defer file.Close()
+
+		fi, err := file.Stat()
+		if err != nil {
+			t.Fatalf("Failed to get file stats: %v", err)
+		}
+
+		if fi.Size() != wantFiles[i].byteCount {
+			t.Errorf("file size mismatch at index %d: got=%d, want=%d", i, fi.Size(), wantFiles[i].byteCount)
+		}
+
+		lineCount := countFileLines(t, file)
 		if lineCount != wantFiles[i].lineCount {
 			t.Errorf("files[%d], %d != %d", i, lineCount, wantFiles[i].lineCount)
-		}
-	}
-
-}
-
-func TestByteSplit(t *testing.T) {
-	input := "abcdefghijklmn"
-	reader := bytes.NewBufferString(input)
-	mode := Byte{UnitByteCount: 4}
-
-	d := t.TempDir()
-
-	// 分割実行
-	if err := mode.Split(reader, d); err != nil {
-		t.Errorf("Failed to split: %v", err)
-	}
-
-	// 生成されたファイル
-	files, err := os.ReadDir(d)
-	if err != nil {
-		t.Errorf("Fail os.ReadDir %+v", err)
-	}
-
-	// ファイルの期待値
-	wantFiles := []struct {
-		name      string
-		byteCount int64
-	}{
-		{
-			name:      "part-byte-1",
-			byteCount: 4,
-		},
-		{
-			name:      "part-byte-2",
-			byteCount: 4,
-		},
-		{
-			name:      "part-byte-3",
-			byteCount: 4,
-		},
-		{
-			name:      "part-byte-4",
-			byteCount: 2,
-		},
-	}
-
-	// ファイルの件数の確認
-	if len(files) != len(wantFiles) {
-		t.Errorf("mismatch the number of files, got=%d, want=%d", len(files), len(wantFiles))
-	}
-
-	// ファイル名確認
-	for i, f := range files {
-		if f.Name() != wantFiles[i].name {
-			t.Errorf("files[%d], %s != %s", i, f.Name(), wantFiles[i].name)
-		}
-	}
-
-	// ファイルサイズを確認する
-	for i, f := range files {
-		file, err := os.Open(filepath.Join(d, f.Name()))
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			t.Errorf("Fail os.Open %v", err)
-		}
-		defer file.Close()
-
-		fi, err := file.Stat()
-		if err != nil {
-			t.Errorf("Fail file.Stat %v", err)
-		}
-		totalSize := fi.Size()
-
-		if totalSize != wantFiles[i].byteCount {
-			t.Errorf("files[%d], %d != %d", i, totalSize, wantFiles[i].byteCount)
-		}
-	}
-
-}
-
-func TestChunkSplit(t *testing.T) {
-	input := "abcdefghijklmn"
-	reader := bytes.NewBufferString(input)
-	mode := Chunk{UnitChunkCount: 2}
-
-	d := t.TempDir()
-
-	// 分割実行
-	if err := mode.Split(reader, d); err != nil {
-		t.Errorf("Failed to split: %v", err)
-	}
-
-	// 生成されたファイルを読み込む
-	files, err := os.ReadDir(d)
-	if err != nil {
-		t.Errorf("Fail os.ReadDir %+v", err)
-	}
-
-	// ファイルの期待値
-	wantFiles := []struct {
-		name      string
-		byteCount int64
-	}{
-		{
-			name:      "part-num-1",
-			byteCount: 7,
-		},
-		{
-			name:      "part-num-2",
-			byteCount: 7,
-		},
-	}
-
-	// ファイルの件数の確認
-	if len(files) != len(wantFiles) {
-		t.Errorf("mismatch the number of files, got=%d, want=%d", len(files), len(wantFiles))
-	}
-
-	// ファイル名確認
-	for i, f := range files {
-		if f.Name() != wantFiles[i].name {
-			t.Errorf("files[%d], %s != %s", i, f.Name(), wantFiles[i].name)
-		}
-	}
-
-	// ファイルサイズを確認する
-	for i, f := range files {
-		file, err := os.Open(filepath.Join(d, f.Name()))
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			t.Errorf("Fail os.Open %v", err)
-		}
-		defer file.Close()
-
-		fi, err := file.Stat()
-		if err != nil {
-			t.Errorf("Fail file.Stat %v", err)
-		}
-		totalSize := fi.Size()
-
-		if totalSize != wantFiles[i].byteCount {
-			t.Errorf("files[%d], %d != %d", i, totalSize, wantFiles[i].byteCount)
 		}
 	}
 }
