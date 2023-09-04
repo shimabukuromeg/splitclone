b3ba015cc802dbb3d0d945e2f0d2e5f00ba641aa
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-09-04 09:49:39 +0900

    テストしやすいようにディレクトリ構成を修正してみた

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## Setup and Running
 
 ```bash
-$ go build -o split .
+$ go build -o split cmd/main.go
 $ ./split -n 3 data/sample.txt
 ```
--- split.go
+++ split.go
@@ -1,9 +1,8 @@
-package main
+package split
 
 import (
 	"bufio"
 	"bytes"
-	"flag"
 	"fmt"
 	"io"
 	"os"
@@ -94,7 +93,7 @@ func (b Byte) Split(reader io.Reader, outputDirName string) error {
 		actualReader = buf
 	}
 
-	chunkCount = int(totalSize) / int(b.UnitByteCount)
+	chunkCount := int(totalSize) / int(b.UnitByteCount)
 	if int(totalSize)%int(b.UnitByteCount) != 0 {
 		chunkCount++
 	}
@@ -176,72 +175,4 @@ func (c Chunk) Split(reader io.Reader, outputDirName string) error {
 		file.Close()
 	}
 	return nil
-}
-
-var (
-	lineCount  int
-	chunkCount int
-	byteCount  int64
-)
-
-const defaultLineCount = 1000
-const defaultChunkCount = 0
-const defaultByteCount = 0
-
-func init() {
-	flag.IntVar(&lineCount, "l", defaultLineCount, "line_count [file]")
-	flag.IntVar(&chunkCount, "n", defaultChunkCount, "chunk_count [file]")
-	flag.Int64Var(&byteCount, "b", defaultByteCount, "byte_count [file]")
-}
-
-func main() {
-	flag.Parse()
-
-	// 指定したオプション数
-	optionCount := 0
-	// 分割モード
-	var mode Mode = Line{UnitLineCount: defaultLineCount}
-	if lineCount != defaultLineCount {
-		optionCount++
-		mode = Line{UnitLineCount: lineCount}
-	}
-	if chunkCount != defaultChunkCount {
-		optionCount++
-		mode = Chunk{UnitChunkCount: chunkCount}
-	}
-	if byteCount != defaultByteCount {
-		optionCount++
-		mode = Byte{UnitByteCount: byteCount}
-	}
-
-	// lineCount, chunkCount, byteCount の値が、デフォルト値以外の値になっているものが2つ以上あったらエラーにする
-	if optionCount > 1 {
-		fmt.Fprintln(os.Stderr, "Please specify only one option")
-		flag.Usage()
-		return
-	}
-
-	var filename string
-	if args := flag.Args(); len(args) > 0 {
-		filename = args[0]
-	}
-
-	var reader io.Reader
-	switch filename {
-	case "":
-		reader = os.Stdin
-	default:
-		f, err := os.Open(filename)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		defer f.Close()
-		reader = f
-	}
-
-	var outputDirName string = ""
-
-	if err := mode.Split(reader, outputDirName); err != nil {
-		fmt.Fprintf(os.Stderr, "fail split file: %v\n", err)
-	}
 }
