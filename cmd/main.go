8358758a99480b8d091db15c9152ce07249a2cf2
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-09-07 22:57:20 +0900

    run関数に分割

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,11 @@
 package main
 
 import (
-	"flag"
 	"os"
-
 	split "splitclone"
 )
 
-var (
-	lineCount  int
-	chunkCount int
-	byteCount  int64
-)
-
-func init() {
-	flag.IntVar(&lineCount, "l", split.DefaultLineCount, "line_count [file]")
-	flag.IntVar(&chunkCount, "n", split.DefaultChunkCount, "chunk_count [file]")
-	flag.Int64Var(&byteCount, "b", split.DefaultByteCount, "byte_count [file]")
-}
-
 func main() {
-	flag.Parse()
 
 	cli := &split.CLI{
 		Stdout: os.Stdout,
@@ -28,5 +13,5 @@ func main() {
 		Stdin:  os.Stdin,
 	}
 
-	cli.Run(lineCount, chunkCount, byteCount)
+	cli.Run(os.Args[1:])
 }
--- run.go
+++ run.go
@@ -19,9 +19,24 @@ const (
 	DefaultByteCount  = 0
 )
 
-func (cli *CLI) Run(lineCount int, chunkCount int, byteCount int64) error {
+var (
+	commandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	lineCount   int
+	chunkCount  int
+	byteCount   int64
+)
+
+func (cli *CLI) Run(args []string) int {
 	// 指定したオプション数
 	optionCount := 0
+
+	commandLine.IntVar(&lineCount, "l", DefaultLineCount, "line_count [file]")
+	commandLine.IntVar(&chunkCount, "n", DefaultChunkCount, "chunk_count [file]")
+	commandLine.Int64Var(&byteCount, "b", DefaultByteCount, "byte_count [file]")
+
+	if err := commandLine.Parse(args); err != nil {
+		fmt.Fprintf(os.Stderr, "can not parse flags: %v\n", err)
+	}
 
 	/*
 		flagをパースした結果から分割方法を確定するロジック
@@ -48,18 +63,21 @@ func (cli *CLI) Run(lineCount int, chunkCount int, byteCount int64) error {
 		flagパッケージでデフォルト値を指定するので、最初から値が入ってて、入ってない場合指定されてない、みたいな判定ができない。
 	*/
 	if optionCount > 1 {
-		// fmt.Fprintln(os.Stderr, "Please specify only one option")
-		flag.Usage()
-		return fmt.Errorf("Please specify only one option: %w", os.Stderr)
-
+		fmt.Fprintln(os.Stderr, "複数のオプションは選択できません")
+		commandLine.Usage()
+		return 1
 	}
 
 	/*
 		ファイル名を取得してる。一番最初決めうちにしてしまってるけどもうちょっとちゃんとしたチェックした方が良さそう？
 	*/
 	var filename string
-	if args := flag.Args(); len(args) > 0 {
+	if args := commandLine.Args(); len(args) > 0 {
 		filename = args[0]
+	} else {
+		fmt.Fprintln(os.Stderr, "ファイルを指定してください")
+		commandLine.Usage()
+		return 0
 	}
 
 	/*
@@ -83,7 +101,8 @@ func (cli *CLI) Run(lineCount int, chunkCount int, byteCount int64) error {
 
 	if err := spliter.Split(reader, outputDirName); err != nil {
 		fmt.Fprintf(os.Stderr, "fail split file: %v\n", err)
+		return 1
 	}
 
-	return nil
+	return 0
 }
